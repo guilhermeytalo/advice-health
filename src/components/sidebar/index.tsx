75562834619e2bb5988a669557b324d85c14f0c1
Guilherme Ytalo <gydaytalo@gmail.com>
2023-08-07 17:33:39 -0300

    added based on navbar of lib

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,96 +1,30 @@
-import React, { useEffect } from 'react';
-import './styles.css';
-import { Nav } from 'react-bootstrap';
+import { Navbar, Nav } from "react-bootstrap";
+import "./styles.css";
 
 function SidebarDefault() {
-  useEffect(() => {
-    const showNavbar = (
-      toggleId: string,
-      navId: string,
-      bodyId: string,
-      headerId: string
-    ) => {
-      const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId),
-        bodypd = document.getElementById(bodyId),
-        headerpd = document.getElementById(headerId);
-
-      // Validate that all variables exist
-      if (toggle && nav && bodypd && headerpd) {
-        toggle.addEventListener('click', () => {
-          // show navbar
-          nav.classList.toggle('show');
-          // change icon
-          toggle.classList.toggle('bx-x');
-          // add padding to body
-          bodypd.classList.toggle('body-pd');
-          // add padding to header
-          headerpd.classList.toggle('body-pd');
-        });
-      }
-    };
-
-    showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header');
-
-    const linkColor = document.querySelectorAll('.nav_link');
-
-    function colorLink(this: HTMLElement) {
-      if (linkColor) {
-        linkColor.forEach((l) => l.classList.remove('active'));
-        this.classList.add('active');
-      }
-    }
-    linkColor.forEach((l) => l.addEventListener('click', colorLink));
-  }, []);
-
   return (
-    <div>
-      <div className="header" id="header">
-        <div className="header_toggle">
-          {' '}
-          <i className="bx bx-menu" id="header-toggle"></i>{' '}
-        </div>
-        <div className="header_img">
-          {' '}
-          <img src="https://i.imgur.com/hczKIze.jpg" alt="" />{' '}
-        </div>
-      </div>
-      <div className="l-navbar" id="nav-bar">
-        <nav className="nav">
-          <div>
-            {' '}
-            <a href="#" className="nav_logo">
-              {' '}
-              <i className="bx bx-layer nav_logo-icon"></i>{' '}
-              <span className="nav_logo-name">BBBootstrap</span>{' '}
-            </a>
-            <div className="nav_list">
-              {' '}
-              <a href="#" className="nav_link active">
-                {' '}
-                <i className="bx bx-grid-alt nav_icon"></i>{' '}
-                <span className="nav_name">Dashboard</span>{' '}
-              </a>{' '}
-              <a href="#" className="nav_link">
-                {' '}
-                <i className="bx bx-user nav_icon"></i>{' '}
-                <span className="nav_name">Consultas</span>{' '}
-              </a>{' '}
-              <a href="#" className="nav_link">
-                {' '}
-                <i className="bx bx-message-square-detail nav_icon"></i>{' '}
-                <span className="nav_name">Agendamentos</span>{' '}
-              </a>{' '}
-            </div>
-          </div>{' '}
-          <a href="#" className="nav_link">
-            {' '}
-            <i className="bx bx-log-out nav_icon"></i>{' '}
-            <span className="nav_name">SignOut</span>{' '}
-          </a>
-        </nav>
-      </div>
-    </div>
+    <Navbar bg="dark" variant="dark" className="l-navbar" id="nav-bar">
+    <Navbar.Brand href="#" className="nav_logo">
+      <i className="bx bx-layer nav_logo-icon"></i>
+      <span className="nav_logo-name">BBBootstrap</span>
+    </Navbar.Brand>
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto nav_list">
+        <Nav.Link href="#" className="nav_link active">
+          <i className="bx bx-grid-alt nav_icon"></i>
+          <span className="nav_name">Dashboard</span>
+        </Nav.Link>
+        <Nav.Link href="#" className="nav_link">
+          <i className="bx bx-user nav_icon"></i>
+          <span className="nav_name">Consultas</span>
+        </Nav.Link>
+        <Nav.Link href="#" className="nav_link">
+          <i className="bx bx-message-square-detail nav_icon"></i>
+          <span className="nav_name">Agendamentos</span>
+        </Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
   );
 }
 
