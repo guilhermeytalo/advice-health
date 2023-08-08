b6567212d4d6164211e845c4c4a421367093e5bf
Guilherme Ytalo <gydaytalo@gmail.com>
2023-08-07 23:01:07 -0300

    feat: added search bar for "agendados"

--- src/pages/agendamento/index.tsx
+++ src/pages/agendamento/index.tsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from 'react';
+import { Container, Row } from 'react-bootstrap';
+import SearchInput from '../../components/search';
+import TableDefault from '../../components/table';
+import { data } from '../../data/data';
+import { TableData } from '../../utils';
+
 export const Agendamento = () => {
+  const lembretes = data.lembretes.consultas;
+  const [searchPatientData, setSearchPatientData] = useState('');
+  const [filteredData, setFilteredData] = useState<TableData[]>([]);
+
+  useEffect(() => {
+    setFilteredData(
+      lembretes.filter(
+        (item) =>
+          item.paciente
+            .toLowerCase()
+            .includes(searchPatientData.toLowerCase()) ||
+          item.medico.toLowerCase().includes(searchPatientData.toLowerCase()) ||
+          item.status.toLowerCase().includes(searchPatientData.toLowerCase())
+      )
+    );
+  }, [searchPatientData]);
+
   return (
-    <div>
-      <h1>Agendamento</h1>
-    </div>
+    <Container>
+      <Row>
+        <SearchInput
+          value={searchPatientData}
+          onChange={setSearchPatientData}
+        />
+        <div>
+          <TableDefault
+            data={filteredData.length > 0 ? filteredData : lembretes}
+          />
+        </div>
+      </Row>
+    </Container>
   );
 };
